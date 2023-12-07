dabed0aec790bc69d04c1860cc109af3461dcd0a
Lê Danh Đức <130637756+ledanhduc@users.noreply.github.com>
2023-12-08 00:47:13 +0700

    Add files via upload

---
+++ href.js
@@ -0,0 +1,17 @@
+
+document.getElementById('ref_user').href = "profile_en.html";
+
+document.getElementById('ref_history').href = "history_en.html";
+
+document.getElementById('ref_analytics').href = "analytics_en.html";
+
+document.getElementById('ref_control').href = "control_en.html";
+
+document.getElementById('ref_statistics').href = "statistics_en.html";
+
+document.getElementById('ref_download').href = "#";
+
+document.getElementById('ref_settings').href = "#";
+
+document.getElementById('ref_add_devices').href = "#";
+
