752e55f7efc63639085722db31f7b40b720a073f
Lê Danh Đức <130637756+ledanhduc@users.noreply.github.com>
2023-12-09 03:18:46 +0700

    Add files via upload

--- in4_en.js
+++ in4_en.js
@@ -1,8 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getDatabase, ref, onValue, onChildAdded, child } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js";
+import { getDatabase, ref, onValue, set, get, onChildAdded, onChildChanged, child } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBsUW2NzEFYcgc32BN0yWdbFKUKxSvgmdI",
+//   authDomain: "sendopt-20057.firebaseapp.com",
+//   databaseURL: "https://sendopt-20057-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "sendopt-20057",
+//   storageBucket: "sendopt-20057.appspot.com",
+//   messagingSenderId: "160375474039",
+//   appId: "1:160375474039:web:cff60b027beaf046194372"
+// };
+
 import firebaseConfig from './firebaseConfig.js';
+
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
@@ -27,24 +38,26 @@ onAuthStateChanged(auth, (user) => {
       id_st.innerText ="ID: " + Id_device;
       handleIdDeviceUpdate(Id_device);
     });
+
+    // onValue(ref(database, `${encodedEmail}/history`), (snapshot) => {
+    //   const data = snapshot.val();
+      // for (const key in data) {
+      //   if (Object.prototype.hasOwnProperty.call(data, key)) {
+      //     console.log(`Key: ${key}, Value: ${data[key]}`);
+      //     // Hiển thị lên giao diện người dùng
+      //     // Ví dụ: document.getElementById('result').innerHTML += `Key: ${key}, Value: ${data[key]} <br>`;
+      //   }
+      // }
+    // });
   }
 });
 
 function handleIdDeviceUpdate(value) {
-console.log(value);
 
-const stCir_Ref = ref(database, `${value}/lamp_1_state`);
-const st_cir = document.getElementById('st_cir')
-onValue(stCir_Ref, (snapshot) => {
-  const stCir = snapshot.val();
-  if (stCir) {
-    st_cir.style.background = "rgba(57,198,92,255)";
-  } else {
-    st_cir.style.background = "rgb(227, 4, 90)";
-  }
-});  
-  
+console.log(value);
 const tempRef = ref(database, `${value}/Frequency`);
+// const tempRef = ref(database, '11971268/Frequency');
+
 onValue(tempRef, (snapshot) => {
   const temp = snapshot.val().toFixed(1);
   // console.log(temp);
@@ -56,15 +69,37 @@ onValue(tempRef, (snapshot) => {
   }
 });
 
+// const tot_preRef = ref(database, `${value}/Voltage`);
+
+// onValue(tot_preRef, (snapshot) => {
+  //   const tot_pre = snapshot.val();
+  //   document.getElementById('tot_pre').textContent = tot_pre + " V";
+  //   document.getElementById('num_pre').style.setProperty('--tot_pre', tot_pre + " V");
+  //   document.getElementById('num_pre').style.setProperty('--dot_pre', `${360 / tot_pre}deg`);
+  
+  // });
+// const stCir_Ref = ref(database, `${value}/lamp_1_state`);
+// const st_cir = document.getElementById('st_cir')
+// onValue(stCir_Ref, (snapshot) => {
+//   const stCir = snapshot.val();
+//   if (stCir) {
+//     st_cir.style.background = "rgba(57,198,92,255)";
+//   } else {
+//     st_cir.style.background = "rgb(227, 4, 90)";
+//   }
+// });
+
 const preRef = ref(database, `${value}/Voltage`);
+
 onValue(preRef, (snapshot) => {
-  const pre = snapshot.val().toFixed(1);
-  document.getElementById('pre').textContent = pre + " V";
-  document.getElementById('pre1').textContent = pre + " V";
+  const pre = snapshot.val();
+  document.getElementById('pre').textContent = pre.toFixed(1) + " V";
+  document.getElementById('pre1').textContent = pre.toFixed(1) + " V";
   document.getElementById('num_pre').style.setProperty('--num_pre', pre);
 });
 
 const humiRef = ref(database, `${value}/Current`);
+
 onValue(humiRef, (snapshot) => {
   const humi = snapshot.val();
   document.getElementById('humi').textContent = humi + ' A';
@@ -73,6 +108,7 @@ onValue(humiRef, (snapshot) => {
 });
 
 const powerRef = ref(database, `${value}/Power`);
+
 onValue(powerRef, (snapshot) => {
   const power = snapshot.val();
   document.getElementById('power').textContent = power + ' W';
@@ -84,6 +120,38 @@ onValue(powerRef, (snapshot) => {
   else document.getElementById("num_power").style.setProperty("--clr-power", "#17c943");
 });
 
+
+
+  var currentMinute;
+  const st_cir = document.getElementById('st_cir');
+  let onlesp;
+
+  // setTimeout(() => {
+  // }, 5000); // Độ trễ 5 giây
+  function sendCurrentMinute() {
+    currentMinute = new Date().getMinutes(); // Cập nhật giá trị currentMinute
+    // console.log(currentMinute);
+  
+    const onlesp_stRef = ref(database, `${value}/onlesp_st`);
+    onValue(onlesp_stRef, (snapshot) => {
+      onlesp = snapshot.val();
+    });
+    // setTimeout(checkOnlesp, 5000); // Đặt thời gian chờ 5 giây trước khi kiểm tra điều kiện
+  }
+  
+  function checkOnlesp() {
+    // console.log(currentMinute);
+    if (onlesp == currentMinute) {
+      st_cir.style.background = "rgba(57, 198, 92, 255)";
+    } else {
+      st_cir.style.background = "rgb(227, 4, 90)";
+    }
+  }
+
+  setInterval(sendCurrentMinute, 1 * 1000);
+  setInterval(checkOnlesp, 8 * 1000);
+  // console.log(currentMinute);
+
 }
 
 onAuthStateChanged(auth, (user) => {
@@ -94,15 +162,26 @@ onAuthStateChanged(auth, (user) => {
   }
 });
 
-var userRead =  sessionStorage.getItem('userses') || localStorage.getItem('user');
+// var userRead =  sessionStorage.getItem('userses') || localStorage.getItem('user');
+// if (userRead === null) {
+//     try {
+//         auth.signOut();
+//     }
+//     catch(error){
+//         console.error(error);
+//       };
+// }
+
+var userRead = sessionStorage.getItem('userses') || localStorage.getItem('user');
 if (userRead === null) {
     try {
         auth.signOut();
-    }
-    catch(error){
+        window.location.replace("login.html"); // Chuyển hướng trở lại trang đăng nhập
+    } catch(error) {
         console.error(error);
-      };
+    }
 }
+
 
 
 
