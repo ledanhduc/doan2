481125fa3d0b6dda4728262200edb182a2117af3
Lê Danh Đức <130637756+ledanhduc@users.noreply.github.com>
2023-12-10 00:10:38 +0700

    Update statistics.js

--- statistics.js
+++ statistics.js
@@ -14,10 +14,9 @@ let encodedEmail;
 const nameuser1 = document.getElementById("nameuser1");
 const avtUser1 = document.getElementById("avt_user1");
 const nos = document.getElementById("nos");
-// nos.value = 40;
+
 const butt_nos = document.getElementById("butt_nos");
-// let cur_nos = 40;
-// let cur_nos;
+
 
 let Id_device;
 onAuthStateChanged(auth, (user) => {
@@ -32,22 +31,12 @@ onAuthStateChanged(auth, (user) => {
     onValue(ref(database, `${encodedEmail}/Id_Device`), (snapshot) => {
       Id_device = snapshot.val();
       handleIdDeviceUpdate(Id_device);
-      // butt_nos.addEventListener('click', function() {
-      //   set(ref(database, `${Id_device}/fre_nos`), nos.value)
-      // });
+
       
     });
   }
 });
 
-// butt_nos.addEventListener('click', function() {
-//   // cur_nos = nos.value;
-//   // handleIdDeviceUpdate(Id_device, cur_nos);
-//   // handleIdDeviceUpdate(Id_device, nos.value);
-// });
-
-
-  // cur_nos = parseInt(nos.value);
 let myChart; // Khai báo biến myChart ở ngoài hàm để lưu trữ biểu đồ
 
 butt_nos.addEventListener('click', function() {
@@ -68,10 +57,6 @@ function handleIdDeviceUpdate(value) {
     // location.reload()
   });
 
-  // butt_nos.addEventListener('click', function() {
-  //   cur_nos = nos.value;
-  //   console.log(cur_nos);
-  // });
 
   const powerData = [];
   const timeData = [];
@@ -80,27 +65,22 @@ function handleIdDeviceUpdate(value) {
 
   const powerRef = ref(database, `${value}/chart_power`);
   onValue(powerRef, (snapshot) => {
-    // const day = new Date().getDate();
+
     const hour = new Date().getHours();
 
     snapshot.forEach((childSnapshot) => {
       const data = childSnapshot.val();
       powerData.push(data.power);
       timeData.push(data.time);
-      // dayData = parseInt(data.day);
+
       hourData = (data.hour);
-      // console.log(hour)
-      // if ((day - dayData) >= 2) {
+
       if ((hour - hourData) >= 5) {
         const childRef = ref(database, `${value}/chart_power/${childSnapshot.key}`);
         remove(childRef);
       }
     });
 
-    // Giới hạn số lượng dữ liệu hiển thị
-    // const displayDataCount = nos.value;
-    // console.log(fre_nos);
-    // console.log(cur_nos);
     const displayDataCount = fre_nos;
     if (powerData.length > displayDataCount) {
       powerData.splice(0, powerData.length - displayDataCount);
@@ -158,69 +138,9 @@ function handleIdDeviceUpdate(value) {
         }
       }
     });
-
-    // Đặt kích thước canvas để phù hợp với kích thước biểu đồ
-    // canvas.style.width = myChart.width;
-    // canvas.style.height = "920px";
-
-    // Đặt thuộc tính CSS overflow-x: auto cho canvas
-    // canvas.style.overflowX = 'auto';
   });
 }
 
-// onAuthStateChanged(auth, (user) => {  
-//   if (user) {
-//     encodedEmail = encodeURIComponent(user.email.replace(/[.@]/g, '_'));
-
-//     onValue(ref(database, `${encodedEmail}/Id_Device`), (snapshot) => {
-//       Id_device = snapshot.val();
-//       handleIdDeviceUpdate(Id_device);
-//     });
-//   }
-// });
-
-// let myChart; // Khai báo biến myChart ở ngoài hàm để lưu trữ biểu đồ
-
-// function handleIdDeviceUpdate(value) {
-//   console.log(value);
-
-//   const powerData = [];
-//   const timeData = [];
-
-//   const powerRef = ref(database, `${value}/chart_power`);
-//   onValue(powerRef, (snapshot) => {
-//     snapshot.forEach((childSnapshot) => {
-//       const data = childSnapshot.val();
-//       powerData.push(data.power);
-//       timeData.push(data.time);
-//     });
-
-//     const ctx = document.getElementById('myChart').getContext('2d');
-//     if (typeof myChart !== 'undefined') {
-//       myChart.destroy();
-//     }
-//     myChart = new Chart(ctx, {
-//       type: 'line',
-//       data: {
-//         labels: timeData,
-//         datasets: [{
-//           label: 'Công suất',
-//           data: powerData,
-//           borderColor: 'rgba(75, 192, 192, 1)',
-//           borderWidth: 1,
-//           fill: false
-//         }]
-//       },
-//       options: {
-//         scales: {
-//           y: {
-//             beginAtZero: true
-//           }
-//         }
-//       }
-//     });
-//   });
-// }
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
@@ -230,15 +150,15 @@ onAuthStateChanged(auth, (user) => {
   }
 });
 
-// var userRead =  sessionStorage.getItem('userses') || localStorage.getItem('user');
-// if (userRead === null) {
-//     try {
-//         auth.signOut();
-//     }
-//     catch(error){
-//         console.error(error);
-//       };
-// }
+var userRead =  sessionStorage.getItem('userses') || localStorage.getItem('user');
+if (userRead === null) {
+    try {
+        auth.signOut();
+    }
+    catch(error){
+        console.error(error);
+      };
+}
 
 
 
